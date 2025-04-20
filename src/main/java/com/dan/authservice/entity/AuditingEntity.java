c03c602d72715a9d092fedbfeafd3f8d72d9e28e
Daniil <astafiev.den@gmail.com>
2025-04-20 20:19:41 +0300

    added JavaDocs

--- src/main/java/com/dan/authservice/entity/AuditingEntity.java
+++ src/main/java/com/dan/authservice/entity/AuditingEntity.java
@@ -11,15 +11,32 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.io.Serializable;
 import java.time.Instant;
 
+/**
+ * Абстрактный класс сущности с поддержкой аудита.
+ * Реализует интерфейс {@link BaseEntity} и включает поля для отслеживания времени создания
+ * и последнего изменения сущности.
+ *
+ * @param <T> тип идентификатора, который должен реализовать {@link Serializable}
+ * @author Даниил Астафьев
+ * @version 1.0
+ */
 @Getter
 @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class AuditingEntity<T extends Serializable> implements BaseEntity<T> {
 
+    /**
+     * Время создания сущности.
+     * Автоматически устанавливается при создании записи.
+     */
     @CreatedDate
     private Instant createdAt;
 
+    /**
+     * Время последнего изменения сущности.
+     * Автоматически обновляется при изменении записи.
+     */
     @LastModifiedDate
     private Instant lastModifiedAt;
 }
--- src/main/java/com/dan/authservice/entity/User.java
+++ src/main/java/com/dan/authservice/entity/User.java
@@ -9,6 +9,13 @@ import java.security.Principal;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Класс сущности пользователя, наследующий класс {@link AuditingEntity} и реализующий интерфейсы {@link UserDetails} и {@link Principal}.
+ * Содержит такие базовые поля, как id, username, email, password, emailVerified.
+ *
+ * @author Даниил Астафьев
+ * @version 1.0
+ */
 @Getter
 @Setter
 @EqualsAndHashCode(of = "email", callSuper = false)
@@ -20,19 +27,34 @@ import java.util.List;
 @Table(name = "users")
 public class User extends AuditingEntity<Long> implements UserDetails, Principal {
 
+    /**
+     * Уникальный идентификатор пользователя.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Имя пользователя (никнейм).
+     */
     @Column(unique = true, nullable = false)
     private String username;
 
+    /**
+     * Электронная почта пользователя.
+     */
     @Column(unique = true, nullable = false)
     private String email;
 
+    /**
+     * Пароль пользователя.
+     */
     @Column(nullable = false)
     private String password;
 
+    /**
+     * Логическое поле, определяющие верифицирован ли пользователь.
+     */
     @Column(name = "email_verified", nullable = false)
     private Boolean emailVerified;
 
